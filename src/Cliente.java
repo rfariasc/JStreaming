b5c9b75557f4526303711cc48d6a1efb2b740a79
Beto <1rfariasc@gmail.com>
2014-11-19 22:56:52 -0300

    TEST1

--- src/Cliente.java
+++ src/Cliente.java
@@ -1,12 +1,36 @@
-
+import java.io.*;
+import java.net.InetAddress;
+import java.net.Socket;
 
 public class Cliente {
+    public static void main(String args[]) throws IOException {
 
-    public static void main(String args[]){
+        int PORT = 12345;
+
+        InetAddress addr = InetAddress.getByName("127.0.0.1");
+
+        System.out.println("addr = " + addr);
+
+        Socket socket = new Socket(addr, PORT);
+
+        try {
+            System.out.println("socket = " + socket);
+            BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+
+            // Output is automatically flushed
+            // by PrintWriter:
+            PrintWriter out = new PrintWriter(new BufferedWriter(new OutputStreamWriter(socket.getOutputStream())), true);
 
 
-
-
+            for (int i = 0; i < 10; i++) {
+                out.println("howdy " + i);
+                String str = in.readLine();
+                System.out.println(str);
+            }
+            out.println("END");
+        } finally {
+            System.out.println("closing...");
+            socket.close();
+        }
     }
-
 }
